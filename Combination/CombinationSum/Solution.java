a93b1cc0b3114e897426d0e5ccc2868f2ada0cd2
zhoujiahao <zhou.jiahao@foxmail.com>
2019-04-23 21:28:32 +0800

    对组合数的第一次优化，利用数组代替String，提高了运行效率

--- Combination/CombinationSum/Solution.java
+++ Combination/CombinationSum/Solution.java
@@ -33,7 +33,6 @@ import java.util.List;
  */
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-
     public static void main(String args[]) {
         int[] candidates = {2, 3, 6, 7};
         int target = 7;
@@ -48,19 +47,15 @@ class Solution {
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        fun(candidates, 0, target, "", target);
+        Integer ans[] = new Integer[1024];
+        fun(candidates, 0, target,ans, target,-1);
         return result;
     }
 
-    public void fun(int[] candidates, int k, int n, String s, int target) {
+    public void fun(int[] candidates, int k, int n, Integer ans[], int target,int len) {
         if (n == 0) {
-            String str[] = s.split("-");
-            int temp[] = new int[str.length];
-            List<Integer> list = new ArrayList<>();
-            for(int i = 1;i<temp.length;i++){
-                temp[i] = Integer.valueOf(str[i]);
-                list.add(temp[i]);
-            }
+            Integer[] newArray = Arrays.copyOfRange(ans, 0, len+1);
+            List<Integer> list = Arrays.asList(newArray);
             result.add(list);
             return;
         }
@@ -69,8 +64,38 @@ class Solution {
             return;
         }
         for (int i = 0; i <= target / candidates[k]; i++) {
-            fun(candidates, k + 1, n - i * candidates[k], s, target);
-            s = s + "-" + candidates[k];
+            fun(candidates, k + 1, n - i * candidates[k], ans, target,len);
+            ans[++len] = candidates[k];
+        }
+    }
+    //这段代码是有问题的。原因在此：list<list>这种类型，他指向的内部list会随内部List的变化而变化
+    public void fun(int[] candidates, int k, int n, List<Integer> list,int target) {
+        List<Integer> list1 = new ArrayList<>();
+        if (n == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        if (n < 0) return;
+        if (k == candidates.length) {
+            return;
+        }
+        for (int i = 0; i <= target / candidates[k]; i++) {
+            fun(candidates, k + 1, n - i * candidates[k], list1, target);
+            list1.add(candidates[k]);
+        }
+    }
+    //大佬的代码
+    private void bt(List<List<Integer>> res, List<Integer> cur, int[] nums, int target, int index, int curSum) {
+        if (curSum == target) {
+            res.add(new ArrayList<>(cur)); return;
+        }
+        for (int i = index; i < nums.length; i ++) {
+            if((curSum + nums[i]) > target) break;
+            cur.add(nums[i]);
+            curSum += nums[i];
+            bt(res, cur, nums, target, i, curSum);
+            cur.remove(cur.size() - 1);
+            curSum -= nums[i];
         }
     }
 }
