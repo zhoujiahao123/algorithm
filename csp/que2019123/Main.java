1fe5bb15cb30dec14a2feb2af50091616f30c96c
jacob <zhou.jiahao@foxmail.com>
2020-02-12 20:24:49 +0800

    化学方程式 100分

--- csp/que2019123/Main.java
+++ csp/que2019123/Main.java
@@ -1,24 +1,28 @@
 package algorithm.csp.que2019123;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    static Map<Integer, Integer> map;
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         boolean ans[] = new boolean[n];
         for (int i = 0; i < n; i++) {
-            map = new HashMap<>();
             String s = input.next();
             String left = s.split("=")[0];
             String right = s.split("=")[1];
             String[] leftArray = left.split("\\+");
             String[] rightArray = right.split("\\+");
-            Map<String, Integer> leftMap = getMap(leftArray);
-            map.clear();
-            Map<String, Integer> rightMap = getMap(rightArray);
+            Map<String, Integer> leftMap = new HashMap<>();
+            Map<String, Integer> rightMap = new HashMap<>();
+            for (String string : leftArray) {
+                new Main().procExpressOnUnit(leftMap, string, 1);
+            }
+            for (String string : rightArray) {
+                new Main().procExpressOnUnit(rightMap, string, 1);
+            }
             ans[i] = judge(rightMap, leftMap);
         }
         for (int i = 0; i < n; i++) {
@@ -40,152 +44,100 @@ public class Main {
         return true;
     }
 
-    private static Map getMap(String[] array) {
-        Map<String, Integer> rightMap = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            int k = 1;
-            //数字可能不是一位
-            int count = 0;
-            if (array[i].charAt(count) >= 48 && array[i].charAt(count) <= 57) {
-                k = array[i].charAt(count) - 48;
-                count++;
-                while (array[i].length() - 1 != count && array[i].charAt(count) >= 48 && array[i].charAt(count) <= 57) {
-                    k = k * 10 + (array[i].charAt(count) - 48);
-                    count++;
+    private void procExpressOnUnit(Map<String, Integer> map, String s, int unit) {
+        int i = 0, n = s.length();
+        int base = unit * getNumberOrDefault(s, i, 1);
+        i = getCurPos(s, i);
+        int pNumber, pElement = i;//记录数字开始的位置和元素开始的位置
+        while (i < n) {
+            if (isAlpha(s.charAt(i))) {
+                pElement = i;//记录元素的起始位置
+                if (i < n - 1 && isLower(s.charAt(i + 1))) {
+                    ++i;
                 }
-            }
-            array[i] = array[i].substring(count);
-            Map<String, Integer> map = compute(array[i], k);
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                if (rightMap.containsKey(entry.getKey())) {
-                    rightMap.put(entry.getKey(), entry.getValue() + rightMap.get(entry.getKey()));
+                pNumber = ++i;
+                String strElement = s.substring(pElement, i);
+                int entry_base = base;
+                if (pNumber != n && isDigit(s.charAt(i))) {
+                    entry_base = base * getNumberOrDefault(s, i, 1);
+                    i = getCurPos(s, i);
+                    pNumber = i;
+                }
+                if (map.containsKey(strElement)) {
+                    map.put(strElement, map.get(strElement) + entry_base);
                 } else {
-                    rightMap.put(entry.getKey(), entry.getValue());
+                    map.put(strElement, entry_base);
                 }
+                if (pNumber == n)
+                    break;
             }
-        }
-        return rightMap;
-    }
-
-    //k是系数
-    private static Map compute(String str, int k) {
-        str = getKuoHaoNum(str);
-        boolean isVisited[] = new boolean[map.size()+1];
-        int count = 0;
-        String pre = "";
-        boolean isFirst = true;
-        Map<String, Integer> maps = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (!isFirst) {
-                if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
-                    //说明是大写字母
-                    int num = maps.containsKey(pre) ? maps.get(pre) : 0;
-                    if (!pre.equals("")) {
-                        maps.put(pre, num + k);
+            if (s.charAt(i) == '(') {
+                int openCount = 1;
+                pElement = ++i;
+                while (true) {
+                    if (s.charAt(i) == '(') {
+                        openCount++;
+                    } else if (s.charAt(i) == ')') {
+                        openCount--;
                     }
-                    pre = "";
-                }
-                if (str.charAt(i) >= 48 && str.charAt(i) <= 57) {
-                    //说明是数字，但数字可能不止一位，要考虑到
-                    int multiply = str.charAt(i) - 48;
-                    while (i != str.length() - 1 && str.charAt(i + 1) >= 48 && str.charAt(i + 1) <= 57) {
-                        i++;
-                        multiply = multiply * 10 + (str.charAt(i) - 48);
-                    }
-                    int num = maps.containsKey(pre) ? maps.get(pre) : 0;
-                    if (!pre.equals("")) {
-                        maps.put(pre, num + multiply * k);
-                    }
-                    pre = "";
-                    if (i == str.length() - 1) {
+                    if (openCount == 0)
                         break;
-                    } else {
-                        i++;
-                    }
+                    i++;
                 }
-                if (str.charAt(i) == '(') {
-                    int num = maps.containsKey(pre) ? maps.get(pre) : 0;
-                    if (!pre.equals("")) {
-                        maps.put(pre, num + k);
-                    }
-                    pre = "";
-                    count++;
-                    while (count!=isVisited.length&&isVisited[count]) {
-                        count++;
-                    }
-                    k = k * map.get(count);
-
+                String strElement = s.substring(pElement, i);
+                pNumber = ++i;
+                procExpressOnUnit(map, strElement, base * getNumberOrDefault(s, pNumber, 1));
+                i = getCurPos(s, i);
+                pNumber = i;
+                if (pNumber < n) {
+                    procExpressOnUnit(map, s.substring(pNumber, n), base);
                 }
-                if (str.charAt(i) == ')') {
-                    int num = maps.containsKey(pre) ? maps.get(pre) : 0;
-                    if (!pre.equals("")) {
-                        maps.put(pre, num + k);
-                    }
-                    pre = "";
-                    k = k / map.get(count);
-                    isVisited[count] = true;
-                    count--;
-                    while (isVisited[count]) {
-                        count--;
-                    }
-                }
-            } else {
-                isFirst = false;
-                if (str.charAt(i) == '(') {
-                    count++;
-                    k = k * map.get(count);
-                }
-            }
-            if (str.charAt(i) != '(' && str.charAt(i) != ')') {
-                pre = pre + str.charAt(i);
-            }
-            if (i == str.length() - 1 && str.charAt(i) != ')') {
-                int num = maps.containsKey(pre) ? maps.get(pre) : 0;
-                if (!pre.equals("")) {
-                    maps.put(pre, num + k);
-                }
+                return;
             }
         }
-        return maps;
     }
 
-    private static String getKuoHaoNum(String str) {
-        if (!str.contains("(")) {
-            return str;
-        }
-
-        int cur = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '(') {
-                cur++;
-                while (map.containsKey(cur)) {
-                    cur++;
-                }
-            } else if (str.charAt(i) == ')') {
-                if (i != str.length() - 1) {
-                    //数字可能不止一位
-                    if (str.charAt(i + 1) >= 48 && str.charAt(i + 1) <= 57) {
-                        i++;
-                        int count = str.charAt(i) - 48;
-                        while (i != str.length() - 1 && str.charAt(i + 1) >= 48 && str.charAt(i + 1) <= 57) {
-                            i++;
-                            count = count * 10 + (str.charAt(i) - 48);
-                        }
-                        map.put(cur, count);
-                        str = str.substring(0, i) + str.substring(i + 1);
-                        i--;
-                    } else {
-                        map.put(cur, 1);
-                    }
-                } else {
-                    map.put(cur, 1);
-                }
-                cur--;
-                while (map.containsKey(cur)) {
-                    cur--;
-                }
+    /**
+     * 获取系数
+     *
+     * @param s          化学式
+     * @param startIndex 算系数开始的位置，因为有可能化学式是这样(Na(3OH)3)
+     * @return
+     */
+    private int getNumberOrDefault(String s, int startIndex, int defaultVal) {
+        int resetVal = 0;
+        for (; startIndex < s.length(); startIndex++) {
+            if (isDigit(s.charAt(startIndex))) {
+                resetVal = resetVal * 10 + (s.charAt(startIndex) - '0');
+            } else {
+                break;
             }
         }
-        return str;
+        return resetVal == 0 ? defaultVal : resetVal;
+    }
+
+    private int getCurPos(String s, int defaultPos) {
+        while (defaultPos < s.length() && isDigit(s.charAt(defaultPos))) {
+            defaultPos++;
+        }
+        return defaultPos;
+    }
+
+    /**
+     * 判断c是否是一个数字
+     *
+     * @param c
+     * @return
+     */
+    private boolean isDigit(char c) {
+        return c >= 48 && c <= 57;
+    }
+
+    private boolean isAlpha(char c) {
+        return c >= 65 && c <= 90 || c >= 97 && c <= 122;
+    }
+
+    private boolean isLower(char c) {
+        return c >= 97 && c <= 122;
     }
 }
